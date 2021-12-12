4d963f52d19193698d78492545d4787dce5f764a
malhar <malharlakdawala@gmail.com>
2021-12-12 12:13:16 +0530

    two number sum

--- Algoexpert/twoNumberSum.py
+++ Algoexpert/twoNumberSum.py
@@ -13,3 +13,22 @@ def twoNumberSum(array, targetSum):
     # Write your code here.
 
 twoNumberSum([4,5,6],6)
+
+
+
+  Write a function that takes in a non-empty array of distinct integers and an
+  integer representing a target sum. If any two numbers in the input array sum
+  up to the target sum, the function should return them in an array, in any
+  order. If no two numbers sum up to the target sum, the function should return
+  an empty array.
+
+
+  Note that the target sum has to be obtained by summing two different integers
+  in the array; you can't add a single integer to itself in order to obtain the
+  target sum.
+
+  You can assume that there will be at most one pair of numbers summing up to
+  the target sum.
+
+two_number_sum([3,5,-4,8,11,1,-1,6],10)
+>> [-1,11]
