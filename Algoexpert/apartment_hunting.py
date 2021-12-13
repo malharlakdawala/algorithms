84a6ee86fc27cb396b251ae5ce6d618c2b3aa502
malhar <malharlakdawala@gmail.com>
2021-12-13 08:32:12 +0530

    appartment hunting

--- Algoexpert/apartment_hunting.py
+++ Algoexpert/apartment_hunting.py
@@ -2,45 +2,50 @@ from timer import timer
 
 
 @timer
-def appartment_hunting(block, reqs):
+def appartment_hunting(blocks, reqs):
+    store_dict = {}
+    store_arr = []
+    i = 0
 
     for block in blocks:
         for req in reqs:
-            if block[req]:
-                pass
-
-#
-
+            for block_item in blocks:
+                if block_item[req] and req not in store_dict:
+                    distance = abs(blocks.index(block_item) - blocks.index(block))
+                    store_dict[req] = distance
+        print("dictionary: ", store_dict)
+        store_arr.append(store_dict)
+        store_dict.clear()
+    print("array: ", store_arr)
 
 
 reqs = ["gym", "school", "store"]
 blocks = [
-  {
-    "gym": false,
-    "school": true,
-    "store": false
-  },
-  {
-    "gym": true,
-    "school": false,
-    "store": false
-  },
-  {
-    "gym": true,
-    "school": true,
-    "store": false
-  },
-  {
-    "gym": false,
-    "school": true,
-    "store": false
-  },
-  {
-    "gym": false,
-    "school": true,
-    "store": true
-  }
+    {
+        "gym": False,
+        "school": True,
+        "store": False
+    },
+    {
+        "gym": True,
+        "school": False,
+        "store": False
+    },
+    {
+        "gym": True,
+        "school": True,
+        "store": False
+    },
+    {
+        "gym": False,
+        "school": True,
+        "store": False
+    },
+    {
+        "gym": False,
+        "school": True,
+        "store": True
+    }
 ]
 
-
-#appartment_hunting(blocks,reqs)
+appartment_hunting(blocks, reqs)
